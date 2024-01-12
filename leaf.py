1de205788bae74dc1c694624f0233362ce0e1b56
Seebles <seebles72@gmail.com>
2024-01-12 15:46:40 -0700

    switing to javascript

--- leaf.py
+++ leaf.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+import math
+#ehh I'll import player later. I think I'm just gonna play this easy.
 pygame.init
 
 screenX = 800
@@ -14,6 +16,8 @@ doExit = False #variable to quit out of game loop
 clock = pygame.time.Clock() #sets up a game clock to regulate game speed
 ticker = 0
 
+leafBlowers = []
+
 def updateTicker():
     global ticker
     if ticker < 60:
@@ -21,21 +25,46 @@ def updateTicker():
     else:
         ticker = 0
 
+def atan2(y, x):
+    num = math.atan2(y, x) * (180/math.pi)
+    if num > 0:
+        return num
+    else:
+        return 180 + (180 - abs(num))
+    
+class temp:
+    def __init__(self, xpos, ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+
+leafBlowers.append(temp(screenX/2, screenY/2))
+
 class leafSpawner:
     def __init__(self, leaves):
         self.xpos = screenX/2 #position for the leaf spawner to be. This doesn't really affect anything but I thought it might be nice to have.
         self.ypos = screenY/2
-        self.colors = []
         self.leaves = []
         for i in range(leaves): #creates a 2d list containing each leaf.
             YY = random.randint(0, screenY)
             self.leaves.append([random.randint(0, screenX), YY, 0, 0]) #xpos, ypos, xvelo, yvelo
-            print("Y = ", YY)
 
-    def draw(self):
+    def draw(self, i):
+            pygame.draw.circle(screen, (0, 255, 0), (self.leaves[i][0], self.leaves[i][1]), 5)
+
+    def update(self):
         for i in range(len(self.leaves)):
-            pygame.draw.circle(screen, (0, 255, 0), (self.leaves[i][1], self.leaves[i][2]), 10)
-            print(self.leaves[i][2])
+            self.applyPhysics(i)
+            self.draw(i)
+
+    def applyPhysics(self, i):
+        for j in range(len(leafBlowers)):
+            angle = atan2(leafBlowers[j].xpos - self.leaves[i][0], leafBlowers[j].ypos - self.leaves[i][1])
+            self.leaves[i][2] += math.cos(angle) * 1.5 #xVelo
+            self.leaves[i][3] += math.sin(angle) * 1.5 #yVelo
+
+    def drag(self, i):
+        self.leaves[i][2] -= 2
+        self.leaves[i][3] -= 2
 
     
 
@@ -52,7 +81,7 @@ while not doExit:
         if event.type == pygame.QUIT:
            doExit = True #lets you quit program
 
-    leeevs.draw()
+    leeevs.update()
 
 
 
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.math import Vector2
 import math
+from pygame import Rect
 pygame.init()
 
 screen = pygame.display.set_mode((800,800))
@@ -9,41 +10,71 @@ screensize = pygame.display.get_window_size()
 class Player:
 
 	def __init__(self):
-		self.pos = Vector2(pygame.mouse.get_pos())
+		self.pos = Rect(0,0,128,128)
 		self.rel = Vector2(0,0)
-		self.vel = Vector2(0,0)
+		self.vel = tuple[0,0]
 
 		self.image = pygame.image.load('resources/placeholder.png')
 		self.offset = Vector2(self.image.get_rect().topleft) - Vector2(self.image.get_rect().midtop)
+
+		self.newMousePos = Vector2(0,0)
+		self.oldMousePos = Vector2(0,0)
+
+		self.correctionangle = 90
 	
+	def mouseShisse(self):
+		self.newMousePos = pygame.mouse.get_pos()
+		self.oldMousePos = self.newMousePos
+
+	def atan2(y, x):
+		num = math.atan2(y, x) * (180/math.pi)
+		if num > 0:
+			return num
+		else:
+			return 180 + (180 - abs(num))
+
 	def update(self):
 		
-		self.vel = pygame.mouse.get_rel()
-		self.rel = pygame.mouse.get_pos() - self.pos
 
-		self.pos += self.vel
+		self.newMousePos = pygame.mouse.get_pos()
+
+		self.rel = self.newMousePos-self.oldMousePos
+
+		print(self.rel)
 		
-	def rotate(self):
-		#angle = (180 / math.pi) * -math.atan2(self.rel.y,self.rel.x)
-		radius, angle = self.rel.as_polar()
-		self.image = pygame.transform.rotate(self.image, -angle)
-		#self.pos+=self.offset
+		self.pos=self.oldMousePos
+
+		self.oldMousePos = self.newMousePos
+		self.draw()
 
 		
-	
 	def draw(self):
-		screen.blit(self.image, self.pos)
+
+		angle = math.degrees(math.atan2(-self.rel.y,self.rel.x)) - self.correctionangle
+
+		transformed_image = pygame.transform.rotate(self.image, angle)
+
+		pygame.math.Vector2.rotate_ip(self.pos,angle)
+		
+		screen.blit(transformed_image, self.newMousePos)
+		#pygame.draw.circle(screen, (255,255,240),self.pos,50)
+
 
 guy = Player()
 clock = pygame.time.Clock()
+bye = False
+guy.mouseShisse()
 
-while True:
+while bye == False:
 	clock.tick(60)
 	screen.fill((0,0,0))
+	for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				bye = True
 
 	guy.update()
-	guy.rotate()
-	guy.draw()
 
 
 	pygame.display.flip()
+
+pygame.quit()
